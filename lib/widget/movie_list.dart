07eb4822ecd4c8bf12937d59ea2c4940260602b7
dkwhyd <dikiwhyd99@gmail.com>
2024-03-11 22:28:39 +0700

    create movie list

---
+++ lib/widget/movie_list.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+class MovieList extends StatefulWidget {
+  const MovieList({super.key});
+
+  @override
+  _MovieListState createState() => _MovieListState();
+}
+
+class _MovieListState extends State {
+  @override
+  Widget build(BuildContext contex) {
+    return Container();
+  }
+}
