9ba72f0c3cc569e46878c10c16bbbc5791a9d9da
dkwhyd <dikiwhyd99@gmail.com>
2024-03-11 20:12:04 +0700

    create http helper

---
+++ lib/helper/http_helper.dart
@@ -0,0 +1,8 @@
+import "package:http/http.dart";
+
+class HttpHelper {
+  final String urlKey = '';
+  final String urlBase = 'https://api.themoviedb.org/3/movie';
+  final String urlUpcoming = '/upcoming?';
+  final String urlLanguage = '&language=en-Us';
+}
