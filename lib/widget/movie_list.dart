b1cda8ff512ebbda25ff1457d5ede83f806c9d18
dkwhyd <dikiwhyd99@gmail.com>
2024-03-11 23:31:48 +0700

    showing movie using card & networkimage

--- lib/widget/movie_list.dart
+++ lib/widget/movie_list.dart
@@ -17,6 +17,10 @@ class _MovieListState extends State {
   int? moviesCount;
   List? movies;
 
+  final String iconBase = 'https://image.tmdb.org/t/p/w92/';
+  final String defaultImage =
+      'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+
   @override
   void initState() {
     helper = HttpHelper();
@@ -26,8 +30,36 @@ class _MovieListState extends State {
 
   @override
   Widget build(BuildContext contex) {
+    NetworkImage image;
     return Container(
-      child: Text('movies count : $moviesCount'),
+      child: movies != null
+          ? ListView.builder(
+              itemCount: moviesCount,
+              itemBuilder: (BuildContext context, int position) {
+                if (movies?[position].posterPath != null) {
+                  image = NetworkImage(iconBase + movies?[position].posterPath);
+                } else {
+                  image = NetworkImage(defaultImage);
+                }
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: image,
+                    ),
+                    title: Text(
+                      movies == null ? '' : movies?[position].title,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    subtitle: Text(
+                      'Released: ${movies == null ? '' : movies?[position].releaseDate} - Vote: ${movies?[position].voteAverage.toString()}',
+                    ),
+                  ),
+                );
+              },
+            )
+          : const Center(
+              child: Text('Loading...'),
+            ),
     );
   }
 
