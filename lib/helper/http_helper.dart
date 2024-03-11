886570b56da1ebb5aac3deaa2c195652b69662ec
dkwhyd <dikiwhyd99@gmail.com>
2024-03-11 20:39:47 +0700

    helper getupcoming

--- lib/helper/http_helper.dart
+++ lib/helper/http_helper.dart
@@ -1,8 +1,24 @@
-import "package:http/http.dart";
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HttpHelper {
-  final String urlKey = '';
-  final String urlBase = 'https://api.themoviedb.org/3/movie';
+  String urlKey = dotenv.env['API_KEY']!;
+  final String urlBase = dotenv.env['BASE_URL']!;
   final String urlUpcoming = '/upcoming?';
   final String urlLanguage = '&language=en-Us';
+
+  Future getUpComing() async {
+    final String upcoming = urlBase + urlUpcoming + urlLanguage;
+    http.Response result = await http.get(upcoming as Uri);
+
+    if (result.statusCode == HttpStatus.ok) {
+      String responseBody = result.body;
+      print(responseBody.toString());
+      return responseBody;
+    } else {
+      return null;
+    }
+  }
 }
